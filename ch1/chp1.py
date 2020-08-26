d2c18aa3cbfbac4e16cf5a60fb3b3d7d6b75f5b2
Lucy Lu <lucylu@Lucys-MacBook-Pro.local>
2020-08-26 11:04:11 -0400

    Chapter 1 notes

---
+++ ch1/chp1.py
@@ -0,0 +1,23 @@
+# 1.3 First Program
+print('Hello, World')
+
+# 1.4 Arithmetic Operators
+x = 3 * 3
+print(x)
+
+# 1.5 Values and types
+y = type(2)
+print(y)
+
+# 1.6 Formal and natural languages
+# syntax is important - how tokens are combined
+# 3+3=6 vs 3 += 3 $ 6
+
+# 1.7 Debugging
+
+# 1.8 Gloassary
+
+# 1.9 Exercises
+# Ex.1
+total_seconds = 42 * 60 + 60
+print(total_seconds)
