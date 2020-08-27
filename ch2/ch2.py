4771ee3e7477f03d98c828b39de9dd29a5e51f57
Lucy Lu <lucylu@Lucys-MacBook-Pro.local>
2020-08-26 20:59:21 -0400

    Chapter 2 complete

---
+++ ch2/ch2.py
@@ -0,0 +1,41 @@
+# 2.1 Assignment statements
+message = 'assiging message'
+
+# 2.2 Variable Names
+"""
+Rules:
+1) Can't begin with number
+2) Usually contains lower case
+3) Can use _
+4) Can't use keywords like assert, break, else
+"""
+
+# 2.3 Expressions and statements
+# Expression: combo of values, variables and operators
+# Statement: creating var or displaying a values
+
+# 2.4 Script mode
+miles = 26.2
+print(miles * 1.61)
+
+# 2.5 Order of operations
+# PEDMAS
+
+# 2.6 String operations
+# String concatenation: using "+" to joing strings together
+# Can also use * to perform string repetition
+
+# 2.7 Comments
+
+# 2.8 Debugging
+"""
+Syntax: structure of a program and rules about that structure
+- matching parentheses
+Runtime: appears after program is Runtime
+Semantic: run without generating error message but won't do the right thing
+"""
+
+# 2.10 Exercises
+# ex. 1
+sphere_volume = (4/3) * 3.14 * (5 ^ 3)
+print(sphere_volume)
